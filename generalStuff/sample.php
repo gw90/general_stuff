f20eac87f3b8ff4c69cbaa9d86383dd7377e72fe
asafmaoz1234 <asafmaoz27@gmail.com>
2014-05-08 10:38:51 +0300

    setup

---
+++ generalStuff/sample.php
@@ -0,0 +1,19 @@
+<?php
+$xml = <<< XML
+<?xml version="1.0" encoding="utf-8" ?>
+<div id="article" class="block large">
+<books>
+    <book>Patterns of Enterprise Application Architecture</book>
+    <book>Design Patterns: Elements of Reusable Software Design</book>
+    <book>Clean Code</book>
+</books>
+<ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+    <li>Four</li>
+    <li><a href="#">Five</a></li>
+</ul>
+</div>
+XML;
+?>
